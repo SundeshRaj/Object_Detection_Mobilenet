5579693283f3a9200109f2b244799ce9c13894dc
Sundesh Raj <sundesh.raj@mavs.uta.edu>
2020-12-02 16:22:30 -0600

    Modified object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -7,15 +7,21 @@ import tarfile
 import tensorflow as tf
 import zipfile
 import cv2
+from PIL import Image
 
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
 
 if __name__ == "__main__":
+
     MODEL_NAME = 'ssd_mobilenet_model'
     PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+    LABELS_FILE = MODEL_NAME + '/mscoco_label_map.pbtxt'
 
     detection_graph = tf.Graph()
     with detection_graph.as_default():
@@ -25,5 +31,15 @@ if __name__ == "__main__":
             graph_def.ParseFromString(serialized_graph)
             tf.import_graph_def(graph_def, name='')
 
-    print(graph_def.ParseFromString)
+    TEST_IMAGES_PATH = 'object_detection/test_images/image1.jpg'
+
+    img = Image.open(TEST_IMAGES_PATH)
+    image_np = load_image_into_numpy_array(img)
+
+    cv2.imshow("Output image", image_np)
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+    print("Done")
 
